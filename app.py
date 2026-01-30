26f7df487850a867e713f039451ba87e7e65ec24
Fernanda <fernandamarquesluizgaspar@gmail.com>
2026-01-30 17:12:05 -0300

    ADD: Páginas complementares e de login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask as F, render_template as rt
+from flask import Flask as F, render_template as rt, request as rq, redirect as red
 
 app = F(__name__)
 
@@ -9,5 +9,23 @@ def index():
 @app.route("/sobremim")
 def sobremim():
     return rt("sobremim.html")
+
+@app.route("/login", methods=["GET"])
+def login():
+    return rt("login.html")
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    usuario = rq.form.get("usuario")
+    senha = rq.form.get("senha")
+    if usuario == "Fernanda" and senha == "123":
+        return rt("comentarios.html")
+    else:
+        #return red("/login")
+        return rt("login.html", erro = "Acesso Negado!")
+
+@app.route("/comentarios")
+def comentarios():
+    return("comentarios.html")
 
 app.run(host="0.0.0.0", port=5050, debug=True)
