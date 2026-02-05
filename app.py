5f0a6157c790c81ca9b28bb0d4c2cf1eb1dfda76
Fernanda <fernandamarquesluizgaspar@gmail.com>
2026-02-05 17:07:01 -0300

    ADD: Página de comentários finalizada.

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask as F, render_template as rt, request as rq, redirect as red
 
 app = F(__name__)
+app.secret_key = "acai"
+
+lc = []
 
 @app.route("/")
 def index():
@@ -21,11 +24,17 @@ def login_post():
     if usuario == "Fernanda" and senha == "123":
         return rt("comentarios.html")
     else:
-        #return red("/login")
         return rt("login.html", erro = "Acesso Negado!")
 
 @app.route("/comentarios")
 def comentarios():
-    return rt("comentarios.html")
+    return rt("comentarios.html", lc = lc)
+
+@app.route("/add_comentarios", methods=["POST"])
+def add_comentario():
+    comentario = rq.form.get("comentario")
+    lc.append(comentario)
+    print(lc)
+    return red("/comentarios")
 
 app.run(host="0.0.0.0", port=5050, debug=True)
